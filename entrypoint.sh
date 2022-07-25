7c13c7a3d6e07b30f748ddff1eb92477d765fc2b
Chekoté <domhnall.tilliere@gmail.com>
2022-07-25 13:18:57 -0500

    Double quote to prevent globbing and word splitting.
    See https://github.com/koalaman/shellcheck/wiki/SC2086

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 replace_in_files() {
-    PHP_CONFIG_FILES=$1
-    VAR_MATCH=$2
+    PHP_CONFIG_FILES="$1"
+    VAR_MATCH="$2"
     REPLACE_VARS=$(printenv | awk -F'=' '{print $1}' | grep -E "^$VAR_MATCH")
 
     # If there are variables to be replace move forward
@@ -15,7 +15,7 @@ replace_in_files() {
 
         # Replace the variable only if it starts with the name of the directive and remove optional ';'
         # Some directives will not have spaces after the directive name. i.e opcache
-        find $PHP_CONFIG_FILES -follow -type f -exec \
+        find "$PHP_CONFIG_FILES" -follow -type f -exec \
             sed -i "s!^;\?$DIRECTIVE \?=[^\n]*!$DIRECTIVE=$VALUE!g" {} \;
       done
     fi
@@ -23,14 +23,14 @@ replace_in_files() {
 
 # Set php.ini options
 for TYPE in cli fpm; do
-    PHP_CONFIG_FILES=/etc/php/$PHP_VERSION/$TYPE/
-    VAR_TYPE=$(echo "PHP_$TYPE" | tr '[:lower:]' '[:upper:]')
+    PHP_CONFIG_FILES="/etc/php/$PHP_VERSION/$TYPE/"
+    VAR_TYPE="$(echo "PHP_$TYPE" | tr '[:lower:]' '[:upper:]')"
 
     # Replace all variables ( prefixed by PHP_TYPE ) on the proper PHP type file
-    replace_in_files $PHP_CONFIG_FILES $VAR_TYPE
+    replace_in_files "$PHP_CONFIG_FILES" "$VAR_TYPE"
 
     # Replace all variables ( prefixed by PHP_ALL )
-    replace_in_files $PHP_CONFIG_FILES "PHP_ALL"
+    replace_in_files "$PHP_CONFIG_FILES" "PHP_ALL"
 done
 
 /usr/local/bin/entrypoint.sh "$@"
